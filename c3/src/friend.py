7a619f0c16c27f12a5e40de389f377b2eb457898
Mario Uriarte <mario.uriarte@gmail.com>
2023-10-24 15:37:10 -0400

    Add diamond inheritance, and mro executed

--- c3/src/friend.py
+++ c3/src/friend.py
@@ -1,6 +1,22 @@
 from built_ins.contact import Contact
+from address_holder import AddressHolder
 
-class Friend(Contact):
-    def __init__(self, name: str, email: str, phone: str) -> None:
-        super().__init__(name, email)        
+# class Friend(Contact):
+#     def __init__(self, name: str, email: str, phone: str) -> None:
+#         super().__init__(name, email)        
+#         self.phone = phone
+
+class Friend(Contact, AddressHolder):
+    def __init__(
+            self,
+            name: str,
+            email: str,
+            phone: str,
+            street: str,
+            city: str,
+            state: str,
+            code: str,
+            ) -> None:
+        Contact.__init__(self, name, email)
+        AddressHolder.__init__(self, street, city, state, code)
         self.phone = phone
